19aa7376102abc23e5290b89cd58de6947029bfb
Giovani Rafael Tavares Braz <giovanibraz02468@gmail.com>
2025-04-22 20:32:52 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Inicio do projeto Next Work</p>
+        <p>Projeto para gestão de trabalhos para escolas/faculdades</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
