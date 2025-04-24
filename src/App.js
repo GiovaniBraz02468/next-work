946e3f5b652dd43220c9bfaa6e0f671fa1a5628f
Giovani Rafael Tavares Braz <giovanibraz02468@gmail.com>
2025-04-23 21:21:04 -0300

    Instalação de bootstrap
    Bootstrap instalado e testado na tela de index.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,18 @@ function App() {
         <p>Projeto para gestão de trabalhos para escolas/faculdades</p>
         <p>Andre-teste</p>
         <p>Giovani-teste</p>
+        <div>
+          <p>Testando bootstrap css</p>
+          <p>Botões:</p>
+          <button type="button" class="btn btn-primary">Primary</button>
+          <button type="button" class="btn btn-secondary">Secondary</button>
+          <button type="button" class="btn btn-success">Success</button>
+          <button type="button" class="btn btn-danger">Danger</button>
+          <button type="button" class="btn btn-warning">Warning</button>
+          <button type="button" class="btn btn-info">Info</button>
+          <button type="button" class="btn btn-light">Light</button>
+          <button type="button" class="btn btn-dark">Dark</button>
+        </div>
       </header>
     </div>
   );
