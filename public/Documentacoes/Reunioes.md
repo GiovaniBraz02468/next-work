7455bf695d311669b7a83162252411bedb4237e9
Giovani Rafael Tavares Braz <giovanibraz02468@gmail.com>
2025-05-04 18:40:48 -0300

    Edição dos recursos de documentação
    Foi adicionado uma nova reunião e um novo modo de navegar entre as reuniões, ficando assim mais focado e específico, sem precisar rolar a tela até encontrar a reunião desejada

---
+++ public/Documentacoes/Reunioes.md
@@ -0,0 +1,6 @@
+# 📅 Atas de Reuniões
+
+## 📌 Lista de reuniões documentadas
+
+- [28/04/2025 - Funcionalidades do Sistema](./reunioes/28-04-2025)
+- [01/05/2025 - Escolha do Back-End](./reunioes/01-05-2025)
--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 import logo from './logo.png';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Inicio do projeto Next Work</p>
-        <p>Projeto para gestão de trabalhos para escolas/faculdades</p>
-        <p>Andre-teste</p>
-        <p>Giovani-teste</p>
-        <p>Gabriel-teste</p>
-        <p>Kaleb-teste</p>
-        <p>Vinicius-teste</p>
-        <div>
-          <p>Testando bootstrap css</p>
-          <p>Botões:</p>
-          <button type="button" class="btn btn-primary">Primary</button>
-          <button type="button" class="btn btn-secondary">Secondary</button>
-          <button type="button" class="btn btn-success">Success</button>
-          <button type="button" class="btn btn-danger">Danger</button>
-          <button type="button" class="btn btn-warning">Warning</button>
-          <button type="button" class="btn btn-info">Info</button>
-          <button type="button" class="btn btn-light">Light</button>
-          <button type="button" class="btn btn-dark">Dark</button>
-          <p>Teste para reunião</p>
-        </div>
-      </header>
+    <div>
+      <p>Página principal</p>
     </div>
   );
 }
